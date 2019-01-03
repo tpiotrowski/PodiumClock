82952acbb6dbe248aba919347c4545417287b721
Tomasz <piotrowski.kat@gmail.com>
2019-01-03 15:07:23 +0100

    Client message Socket Add Console app for socket testing

--- Client/src/ItSoft.ClientService/CloclMessageClient.cs
+++ Client/src/ItSoft.ClientService/CloclMessageClient.cs
@@ -8,55 +8,133 @@ namespace ItSoft.ClientService
 {
     public class SocketClockMessageClient : IClockMessageClient<byte[]>
     {
+        private readonly string _ipAddress;
+        private readonly int _port;
         public event EventHandler<ClockDataEventArgs<byte[]>> DataReceived;
+        public event EventHandler ClientDisconnected;
+        public event EventHandler ClientConnected;
         private Timer _timer = null;
         private Timer _watchDog = null;
-        private Socket _socket = null;
-        private IPHostEntry _ipHostInfo;
-        private IPAddress _address;
-        private IPEndPoint _endPoint;
 
-        public int ReadPeriod { get; set; } = 1000;
-        public int WatchDogPeriod { get; set; } = 1000;
+        private Socket _socket = null;
+
+        // private IPHostEntry _ipHostInfo;
+        //private IPAddress _address;
+        //private IPEndPoint _endPoint;
+
+        public int ReadPeriod { get; set; } = 500;
+        public int WatchDogPeriod { get; set; } = 10000;
 
         public SocketClockMessageClient(string ipAddress, int port)
         {
-            _ipHostInfo = Dns.GetHostEntry(ipAddress);
-            _address = _ipHostInfo.AddressList.First();
-            _endPoint = new IPEndPoint(_address, port);
+            _ipAddress = ipAddress;
+            _port = port;
+            /*_ipHostInfo = Dns.GetHostEntry(ipAddress);
+            _address = _ipHostInfo.AddressList.First();*/
         }
+
+        public void StartClient()
+        {
+            Connect();
+        }
+
+        bool SocketConnected(Socket s)
+        {
+            if (s == null) return false;
+
+            if (!s.Connected)
+            {
+                return false;
+            }
+            else
+            {
+                bool part1 = s.Poll(1000, SelectMode.SelectRead);
+                bool part2 = (s.Available == 0);
+                if (part1 && part2)
+                    return false;
+                else
+                    return true;
+            }
+        }
+
+       
+
 
         private void Connect()
         {
-            if (_socket == null)
+            var socketConnected = SocketConnected(_socket);
+
+            if (socketConnected)
             {
-                _socket = new Socket(_address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                return;
             }
-
-            if (_socket.Connected) return;
-
-            _socket.BeginConnect(_endPoint, res =>
+            
+            var ipEndPoint = new IPEndPoint(IPAddress.Parse(_ipAddress), _port);
+            _socket = new Socket(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            _socket.BeginConnect(ipEndPoint, res =>
             {
                 var socket = (Socket) res.AsyncState;
-                if (socket.Connected)
+
+                if (SocketConnected(socket))
                 {
+                    ClientConnected?.Invoke(this,EventArgs.Empty);
                     _watchDog?.Dispose();
-                    _timer = new Timer(ReadData, _socket, 0, WatchDogPeriod);                  
+                    _watchDog = null;
+
+
+                    _timer = new Timer(state =>
+                    {
+                        ReadData(state);
+
+                        _timer?.Change(ReadPeriod, Timeout.Infinite);
+                    }, _socket, 0, Timeout.Infinite);
                 }
                 else
                 {
-                    _timer.Dispose();
-                    _watchDog = new Timer(state => { Connect(); }, _socket, 0, WatchDogPeriod);
+                    RunConnectionWatchDog();
                 }
             }, _socket);
+        }
+
+        private void RunConnectionWatchDog()
+        {
+            ClientDisconnected?.Invoke(this,EventArgs.Empty);
+
+            _timer?.Dispose();
+            _timer = null;
+            if (_watchDog == null)
+                _watchDog = new Timer(state =>
+                {
+                    Connect();
+
+                    _watchDog?.Change(WatchDogPeriod, Timeout.Infinite);
+                }, null, 0, Timeout.Infinite);
         }
 
         private void ReadData(object state)
         {
             if (state is Socket socket)
             {
+                if (!SocketConnected(socket))
+                {
+                    RunConnectionWatchDog();
+                    return;
+                }
+
+                var buffer = new byte[1024];
+
                 try
                 {
+                    var byteReceive = _socket.Receive(buffer);
+
+                    if (byteReceive > 0)
+                    {
+                        var message = new byte[byteReceive];
+
+                        Array.Copy(buffer, message, byteReceive);
+
+                        DataReceived?.Invoke(this, new ClockDataEventArgs<byte[]>() {Message = message});
+                    }
                 }
                 catch (Exception e)
                 {
---
+++ Client/test/ItSoft.ClientService.TestApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace ItSoft.ClientService.TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SocketClockMessageClient client = new SocketClockMessageClient("127.0.0.1", 8811);
+            client.StartClient();
+            client.ClientConnected += (sender, eventArgs) => Console.WriteLine("Client connected");
+            client.ClientDisconnected += (sender, eventArgs) => Console.WriteLine("Client disconnected");
+            client.DataReceived += Client_DataReceived;
+
+            while (true)
+            {
+                
+            }
+        }
+
+
+        private static void Client_DataReceived(object sender, ClockDataEventArgs<byte[]> e)
+        {
+            UTF8Encoding utf8 = new UTF8Encoding();
+
+            var s = utf8.GetString(e.Message);
+
+            Console.WriteLine(s);
+            
+        }
+    }
+}
