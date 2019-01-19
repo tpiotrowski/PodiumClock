ab054ea1083c6974edf7f0da3eb516ca3b446ecf
Tomasz <piotrowski.kat@gmail.com>
2019-01-19 12:13:26 +0100

    Refactoring

---
+++ Client/src/ClockClient.Android.Services/ClockClientService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.OS;
+
+namespace ClockClient.Android.Services
+{
+    [Service(IsolatedProcess = true)]
+    public class ClockClientService : Service
+    {
+        public override IBinder OnBind(Intent intent)
+        {
+            return null;
+        }
+
+        
+        //Service is created
+        public override StartCommandResult OnStartCommand(Intent intent, StartCommandFlags flags, int startId)
+        {
+            return base.OnStartCommand(intent, flags, startId);
+        }
+
+        public override bool OnUnbind(Intent intent)
+        {
+            return base.OnUnbind(intent);
+        }
+
+        //Stop servie is called 
+        public override void OnDestroy()
+        {
+            base.OnDestroy();
+        }
+    }
+}
